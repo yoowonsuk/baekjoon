d9633074b09ed0a575c7d204c75efbd13f0d8238
yoowonsuk <sam2yuk@naver.com>
2021-04-08 14:25:37 +0900

    1007

--- 1007.c
+++ 1007.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+//#include <limits.h>
+#include <float.h>
+#include <math.h>
 
 typedef struct
 {
@@ -6,14 +9,66 @@ typedef struct
   int y;
 } pos;
 
+pos data[20];
+int c[20];
+double result;
+int N;
+
+double min(double a, double b)
+{
+	return a < b ? a : b;
+}
+
+void solve(int cnt, int index)
+{
+	if(cnt == N/2)
+	{
+		int i;
+		double x, y;
+		x = y = 0.0;
+		for(i=0; i<N; i++)
+			if(c[i])
+			{
+				x -= data[i].x;
+				y -= data[i].y;
+			}
+			else
+			{
+				x += data[i].x;
+				y += data[i].y;
+			}
+		result = min(result, sqrt(x*x+y*y));
+		return;
+	}
+	if(index == N)
+		return;
+
+	solve(cnt, index+1);
+	c[index] = 1;
+	solve(cnt + 1, index + 1);
+	c[index] = 0;
+}
+
+
 int main(void)
 {
-  int total, count;
-  int num;
+	int i, j;
+	int T;
+  
+	scanf("%d", &T);
+  
+	for(i=0; i<T; i++)
+	{
+		scanf("%d", &N);
+		
+		for(j=0; j<N; j++)
+			scanf("%d %d", &data[j].x, &data[j].y);
 
-  pos data[20];
-  int sign[20];
-
-
+		result = DBL_MAX;
+		solve(0, 0);
+		printf("%.6f\n", result);
+	}
+	return 0;
+}
 
 

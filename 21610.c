b8aa6dc3cae856e27d31aa6ac46543dc402b17b4
yoowonsuk <sam2yuk@naver.com>
2021-04-28 10:28:08 +0900

    21610.c samsung rain.....}

---
+++ 21610.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NMAX 50
+#define MMAX 100
+
+int dx[] = {0, -1, -1, -1, 0, 1, 1, 1};
+int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+typedef struct
+{
+	int pond, cloud;
+} MAP;
+
+MAP map[NMAX][NMAX];
+MAP temp[NMAX][NMAX];
+
+int d[MMAX];
+int s[MMAX];
+
+int N, M;
+int ans;
+int which;
+int cloudnum = 4;
+
+int isBound(int x, int y)
+{
+	return x>=0 && y>=0 && x<N && y<N;
+}
+
+
+void cloudmove()
+{
+	int tempx, tempy;
+	int i, j;
+	int num = 0;
+
+	for(i=0; i<N; i++)
+		for(j=0; j<N; j++)
+			if(map[i][j].cloud)
+			{
+				tempx = i + dx[d[which]] * s[which];
+				tempy = j + dy[d[which]] * s[which];
+
+				while(tempx < 0)
+					tempx += N;
+				while(tempx >= N)
+					tempx -= N;
+
+				while(tempy < 0)
+					tempy += N;
+				while(tempy >= N)
+					tempy -= N;
+
+				map[i][j].cloud = 0;
+				temp[tempx][tempy].cloud = 1;
+				num++;
+
+				if(num == cloudnum)
+				{
+					int i, j;
+					num = 0;
+					for(i=0; i<N; i++)
+						for(j=0; j<N; j++)
+						{
+							if(temp[i][j].cloud)
+							{
+								map[i][j].cloud = temp[i][j].cloud;
+								num++;
+								if(num == cloudnum)
+								{
+									memset(temp, 0, sizeof(temp));
+									which++;
+									return;
+								}
+							}
+						}
+				}
+
+			}
+}
+				
+void rain(void)
+{
+	int i, j, k;
+	int num = 0;
+	int tempx, tempy;
+
+	for(i=0; i<N; i++)
+		for(j=0; j<N; j++)
+			if(map[i][j].cloud)
+			{
+				map[i][j].pond++;
+				for(k=0; k<4; k++)
+				{
+					tempx = i + dx[2*k + 1];
+					tempy = j + dy[2*k + 1];
+					if(!isBound(tempx, tempy))
+						continue;
+					else if(map[tempx][tempy].pond || map[tempx][tempy].cloud)
+						map[i][j].pond++;
+				}
+
+				num++;
+				if(num == cloudnum)
+					return;
+			}
+
+	/*
+	num = 0;
+	for(i=0; i<N; i++)
+		for(j=0; j<N; j++)
+			if(map[i][j].cloud)
+			{
+				for(k=0; k<4; k++)
+				{
+					tempx = i + dx[2*k + 1];
+					tempy = j + dy[2*k + 1];
+					if(!isBound(tempx, tempy))
+						continue;
+					else if(map[tempx][tempy].pond)
+						map[i][j].pond++;
+				}
+				num++;
+				if(num == cloudnum)
+					return;
+			}
+	*/
+}
+
+void makecloud()
+{
+	int i, j;
+	cloudnum = 0;
+	for(i=0; i<N; i++)
+		for(j=0; j<N; j++)
+			if(map[i][j].cloud)
+				map[i][j].cloud = 0;
+			else if(!map[i][j].cloud && map[i][j].pond>1)
+			{
+				map[i][j].cloud = 1;
+				map[i][j].pond -= 2;
+				cloudnum++;
+			}
+
+	/*
+	{
+int i, j;
+for(i=0; i<N; i++)
+{
+	for(j=0; j<N; j++)
+		printf("%d(%d) ", map[i][j].pond, map[i][j].cloud);
+	printf("\n");
+}
+printf("\n");
+}
+*/
+}
+
+void solve()
+{
+	int i;
+	map[N-1][0].cloud = map[N-2][0].cloud = map[N-1][1].cloud = map[N-2][1].cloud = 1;
+
+	for(i=0; i<M; i++)
+	{
+		cloudmove();
+		rain();
+		makecloud();
+	}
+}
+
+int cal()
+{
+	int i, j;
+
+	for(i=0; i<N; i++)
+		for(j=0; j<N; j++)
+			ans += map[i][j].pond;
+}
+
+
+
+int main(void)
+{
+	int i, j;
+	scanf("%d %d", &N, &M);
+
+	for(i=0; i<N; i++)
+		for(j=0; j<N; j++)
+			scanf("%d", &map[i][j].pond);
+
+	for(i=0; i<M; i++)
+	{
+		scanf("%d %d", &d[i], &s[i]);
+		d[i]--;
+	}
+
+	solve();
+	cal();
+	printf("%d", ans);
+	return 0;
+}

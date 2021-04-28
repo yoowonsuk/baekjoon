e2a60aee3b89b4b853adb0cdc8a8bf6c1b98059c
yoowonsuk <sam2yuk@naver.com>
2021-04-28 21:08:59 +0900

    21611 finish

--- 21611.c
+++ 21611.c
@@ -6,6 +6,7 @@
 
 int map[NMAX][NMAX];
 int arr[NMAX*NMAX];
+int temp[NMAX*NMAX];
 
 int N, M;
 
@@ -19,13 +20,21 @@ int which;
 int where;
 int num;
 int ans;
+int done;
 
 void score(int x, int y, int which) // which = 1 means map, which = 2 means arr & y -> count
 {
-	if(which == 1)
-		ans+=map[x][y];
-	else if(which == 2)
+	//if(which == 1)
+	//	ans+=map[x][y];
+	//else if(which == 2)
 		ans+=arr[x]*y;
+
+	/*
+	if(which == 1)
+		printf("ans: %d, map[x][y]: %d, x: %d, y: %d\n", ans, map[x][y], x, y);
+	else
+		printf("ans: %d, arr[x]: %d, x: %d, y: %d\n", ans, arr[x], x, y);
+	*/
 }
 
 void maprint()
@@ -69,29 +78,67 @@ void blizzard()
 		y += dy[d[which]];
 		if(!map[x][y])
 			break;
-		score(x, y, 1);
+		//score(x, y, 1);
 		map[x][y] = -1;
 	}
 	which++;
 }
 
-void four()
+int four()
 {
 	int i, j;
 	int last;
+	int count = 0;
 
 	for(i=0; i<=num-3; i++)
 		if(arr[i] == arr[i+1] && arr[i] == arr[i+2] && arr[i] == arr[i+3])
 		{
+			done = 0;
 			last = i+4;
 			while(arr[i] == arr[last])
 				last++;
-			score(arr[i], last-i, 2);
+			//printf("which: %d, i: %d, last: %d\n", which, i, last);
+			score(i, last-i, 2);
+			count += (last-i);
 			for(j=i; j<last; j++)
 				arr[j] = -1;
-			i = last-1;
+			i = last;
 		}	
+	j = 0;
+	for(i=0; i<num; i++)
+		if(arr[i] != -1)
+			arr[j++] = arr[i];
+	//printf("num: %d, count: %d\n", num, count);
+	num -= count;
 }
+
+void rearrange()
+{
+	int i=0, j;
+	int tempwhich=0;
+
+	//printf("num: %d\n", num);
+	while(i<num)
+	{
+		j = i; // j is init
+		while(arr[i] == arr[i+1] && i < num)
+			i++;
+		//printf("j: %d, i: %d, arr[j]: %d\n", j, i, arr[j]);
+		//printf("i: %d, arr[i]: %d, arr[i+1]: %d\n", i, arr[i], arr[i+1]);
+		temp[tempwhich++] = i-j+1;
+		temp[tempwhich++] = arr[j];
+		if(tempwhich == N*N-1)
+			break;
+		i++;
+	}
+	num = tempwhich;
+	//printf("num: %d\n", num);
+	for(i=0; i<num; i++)
+		arr[i] = temp[i];
+	arr[i] = 0;
+}
+
+
 
 void mapping(int what) // what = 1 map -> arr, what = 2 arr -> map
 {
@@ -182,11 +229,18 @@ void solve()
 
 	for(i=0; i<M; i++)
 	{
-		blizzard();
-		mapping(1);
-		four();
+		blizzard(); // good
+		mapping(1); // good
+		done = 0;
+		while(!done)
+		{
+			done = 1;
+			four(); // good
+		}
 		memset(map, 0, sizeof(map));
+		rearrange();
 		mapping(2);
+		maprint();
 	}
 }
 
